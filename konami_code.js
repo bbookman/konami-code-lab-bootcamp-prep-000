f8976e1a145eb0454a7df52b0e506b305bb7fb01
Bruce Bookman <bruce.bookman@gmail.com>
2018-11-21 02:35:01 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,28 @@ const codes = [
 ];
 
 function init() {
-  // your code here
-}
+  const body = document.querySelector('body');
+  body.addEventListener('keydown', function(e)
+    { 
+       var keyPressCount = 0;
+       const keyPressMax = codes.length;
+       
+       while (keyPressCount <= keyPressMax -1){
+         if (e.key != codes[keyPressCount]){
+           alert("BAD");
+         } else {
+             if (keyPressCount == (keyPressMax - 1)) {
+             alert("Woo hoo");
+               
+             } else {
+               keyPressCount += 1; 
+               console.log(keyPressCount)
+             }
+          } 
+      
+      }
+  })//addEventListener
+}//init()
+
+console.log("Hello")
+init()
